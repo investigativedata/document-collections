0f5b2ad9bf23e3f85f6ae8e15d4809c2e0c115fb
Simon Wörpel <simon.woerpel@pm.me>
2023-02-12 04:40:23 +0100

    Use time limit for github actions

--- common/memorious.py
+++ common/memorious.py
@@ -1,16 +1,34 @@
-from pathlib import Path
+from datetime import datetime
 
-from memorious.operations.store import _get_directory_path, directory
+from dateparser import parse
+from memorious.core import conn
+from memorious.operations.store import directory
 from servicelayer import env
+from servicelayer.cache import make_key
 
 LIMIT = env.to_int("LIMIT")
+TIME_LIMIT = env.to_int("TIME_LIMIT")
 
 
 def store(context, data):
     directory(context, data)
+    conn.incr(make_key(context.crawler, "run", context.run_id, "documents"))
+
+    if TIME_LIMIT:
+        start = parse(
+            conn.get(make_key(context.crawler, "run", context.run_id, "start"))
+        )
+        delta = (datetime.utcnow() - start).seconds / 60
+        if delta > TIME_LIMIT:
+            context.log.info(
+                "Time limit reached: %d / %d minutes" % (delta, TIME_LIMIT)
+            )
+            context.crawler.cancel()
+
     if LIMIT:
-        path = Path(_get_directory_path(context))
-        files = len(list(path.glob("*.json")))
-        if files >= LIMIT:
-            context.log.info("Limit reached: %d" % files)
+        documents = int(
+            conn.get(make_key(context.crawler, "run", context.run_id, "documents"))
+        )
+        if documents >= LIMIT:
+            context.log.info("Limit reached: %d documents" % documents)
             context.crawler.cancel()
