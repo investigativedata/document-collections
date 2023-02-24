ec7f7cdc29b25be419b9c43c22301a0e31823bc9
Simon Wörpel <simon.woerpel@pm.me>
2023-02-24 13:32:50 +0100

    Reorder upload steps

--- upload.sh
+++ upload.sh
@@ -3,12 +3,11 @@
 # args:
 # 1 dataset name
 
-# index
-aws s3 --endpoint-url https://minio.ninja cp --no-progress data/export/index.json s3://catalog.investigativedata.org/$1/
-
-# memorious db
-aws s3 --endpoint-url https://minio.ninja cp --no-progress data/datastore.sqlite3 s3://catalog.investigativedata.org/$1/
+# documents (do first bc if that fails, we should not update metadata)
+aws s3 --endpoint-url https://minio.ninja sync --no-progress data/store/$1 s3://catalog.investigativedata.org/$1/documents
 # entities
 aws s3 --endpoint-url https://minio.ninja cp --no-progress data/export/entities.ftm.json s3://catalog.investigativedata.org/$1/entities/`date +"%Y-%m-%d_%H_%M_%S"`.ftm.json
-# documents
-aws s3 --endpoint-url https://minio.ninja sync --no-progress data/store/$1 s3://catalog.investigativedata.org/$1/documents
+# memorious db
+aws s3 --endpoint-url https://minio.ninja cp --no-progress data/datastore.sqlite3 s3://catalog.investigativedata.org/$1/
+# index
+aws s3 --endpoint-url https://minio.ninja cp --no-progress data/export/index.json s3://catalog.investigativedata.org/$1/
